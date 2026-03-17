c32f291500699f62be8f75ebdb653e4aa793d1db
AlexArguetaNet <alexarguetanet@gmail.com>
2026-03-17 18:08:26 -0400

    Implemented ping feature
    Created a function in ping.py that runs the
    terminal command to ping the loopback IP address
    and Google's IP address 8.8.8.8. A user is also
    able to enter another IP address as a command-line
    argument.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from utils.ping import ping_ip
+import sys
 
-print("Hello, world!")
+def main():
+
+    ip_addresses = ["127.0.0.1", "8.8.8.8"]
+    other_ip = sys.argv[1:]
+
+    if other_ip:
+        ip_addresses = ip_addresses + other_ip
+
+    for ip in ip_addresses:
+        ping_ip(ip)
+
+if __name__ == "__main__":
+    main()
---
+++ utils/ping.py
@@ -0,0 +1,21 @@
+import subprocess
+import platform
+
+def get_ping_flag():
+    os_name = platform.system().lower()
+
+    if os_name == "windows":
+        return "-n"
+    else:
+        return "-c"
+    
+def ping_ip(ip_address):
+    flag = get_ping_flag()
+
+    command = ["ping", flag, "4", ip_address]
+    print(f"Pinging {ip_address}. One moment please.")
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    print(result.stdout)
+
+    return result.returncode
