a90f52e2d39ca567d8afcb8a464a02c116ff1e38
Alejandro "Alex" Argueta <90729486+AlexArguetaNet@users.noreply.github.com>
2026-03-18 11:44:55 -0400

    Implemented ping feature (#1)
    * Implemented ping feature
    
    Created a function in ping.py that runs the
    terminal command to ping the loopback IP address
    and Google's IP address 8.8.8.8. A user is also
    able to enter another IP address as a command-line
    argument.
    
    * Changed variable names
    
    Changed variables with that contained "ip"
    to "host" since the app can accept IP addresses
    and domain names as arguments to ping.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from utils.ping import ping_host
+import sys
 
-print("Hello, world!")
+def main():
+
+    hosts = ["127.0.0.1", "8.8.8.8"]
+    other_hosts = sys.argv[1:]
+
+    if other_hosts:
+        hosts = hosts + other_hosts
+
+    for host in hosts:
+        ping_host(host)
+
+if __name__ == "__main__":
+    main()
