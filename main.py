a77f3f49797b0075da6c04164ec9035974daa170
Alejandro "Alex" Argueta <90729486+AlexArguetaNet@users.noreply.github.com>
2026-03-19 11:08:46 -0400

    Running the ping command on the default gateway (#2)
    * Created functions to get default get and loopback IP addresses
    
    Created a new module called get_ip_addrs.py. The module contains
    two functions: get_default_gateway_ip() and get_loopback_ip() which
    returns the IP addresses for the default gateway and loopback interface
    respectfully.
    
    * Edited feature to get IP addresses for loopback and default gateway
    
    Changed how to IP addresses for the default gateway and loopback
    are obtained. So far this only works on Linux. Instead of using
    the netifaces2 library, the subprocess module is used to run the
    command to get the IP addresses according to the operating system
    being used. The IP address is then extracted from the text response
    stored in the result variable after subprocess.run executes.
    
    * Can get the default gateway and loopback IP addresses on Windows
    
    The functions in get_ip_addrs.py can now run on Windows systems. The Windows
    commands to display the IP addresses of the default gateway and loopback are
    passed into the run function and then parsed to get the IP address strings.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from utils.ping import ping_host
+from utils.get_ip_addrs import get_default_gateway_ip, get_loopback_ip
 import sys
 
 def main():
 
-    hosts = ["127.0.0.1", "8.8.8.8"]
+    default_gateway_ip = get_default_gateway_ip()
+    loopback_ip = get_loopback_ip()
+
+    #These hosts will be tested by default
+    hosts = [loopback_ip, default_gateway_ip,"8.8.8.8"]
+
+    # Other hosts entered as command-line arguments
     other_hosts = sys.argv[1:]
 
     if other_hosts:
         hosts = hosts + other_hosts
 
     for host in hosts:
-        ping_host(host)
+        print(ping_host(host))
+
+
 
 if __name__ == "__main__":
     main()
