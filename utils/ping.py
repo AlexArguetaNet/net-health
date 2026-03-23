36ba0a3e743eef9a9d7950bf91e56d641cbe0f6e
AlexArguetaNet <alexarguetanet@gmail.com>
2026-03-23 13:50:22 -0400

    Obtained new data from ping test
    The function ping_results in ping.py gets the packet loss
    percentage, average latency and the jitter amount and returns
    these values. The function grade_connection analysis the results
    and return a score with a single-word description describing the
    connection strength. For now, this is only supported on Linux.

--- utils/ping.py
+++ utils/ping.py
@@ -19,7 +19,7 @@ def ping_host(host):
 
     return result
 
-def ping_stats(host):
+def ping_results(host):
 
     try:
         
@@ -34,7 +34,7 @@ def ping_stats(host):
                 if "Minimum" in line:
                     time_line = line.strip().replace(" ", "").split(",")
             
-            packet_lost = packets_line[2].split("=")[1][0]
+            packet_loss = packets_line[2].split("=")[1][0]
             min_time = time_line[0].split("=")[1].split("m")[0]
 
         elif os_name == "linux":
@@ -44,12 +44,53 @@ def ping_stats(host):
                 if "rtt" in line:
                     time_line = line.strip().replace(" ", "").split("=")
             
-            packet_lost = packets_line[2][0]
+            packet_loss = packets_line[2][0]
             min_time = time_line[1].split("/")[0]
+            avg_latency = time_line[1].split("/")[1]
+            max_time = time_line[1].split("/")[2]
 
-            min_time = float(min_time)
-
-        return [int(packet_lost), int(min_time)]
+        return int(packet_loss), float(avg_latency), float(max_time) - float(min_time)
 
     except Exception as e:
         return None
+
+def grade_connection(packet_loss, avg_latency, jitter):
+
+    score = 100
+
+    # Deduct for packet loss: 1% drops 20 points
+    loss_penalty = packet_loss * 20
+    score -= loss_penalty
+
+    # Deduct for latency: 
+    # Every 10ms after 50ms costs 1 point
+    if avg_latency > 50:
+        latency_penalty = (avg_latency - 50) / 10
+        score -= latency_penalty
+
+    # Deduct for jitter
+    if jitter > 5:
+        jitter_penalty = ((jitter - 5) / 5) * 5
+        score -= jitter_penalty
+
+    # Make sure the score is between 0 and 100
+    score = max(0, min(100, score))
+
+    # Assign description to grade
+    description = ""
+
+    if packet_loss >= 100:
+        description = "FAILED"
+    else:
+        if score >= 90:
+            description = "EXCELLENT"
+        elif score >= 80:
+            description = "GOOD"
+        elif score >= 60:
+            description = "FAIR"
+        else:
+            description = "POOR"
+    
+    return score, description
+
+
